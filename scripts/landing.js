571202259f93a15a3663b3c01801277757538b73
Ryan Arakawa <arakawa.ryan@gmail.com>
2016-12-04 15:13:49 -0600

    Checkpont 9 Assignment: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,22 @@
+//creates array of elements with "point" class
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function (points) {
-    
-    var revealPoint = function (array) {
-        forEach(array, myCallback);
-    }
-    revealPoint(pointsArray);
+//style selling points
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scale(1) translateY(0)"
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
-window.onload = function () {
+
+//call forEach function to iterate through the three members of the pointsArray
+var animatePoints = function(array) {
+    forEach(array, revealPoint);
+};
+
+//controls events for window onload, 
+window.onload = function() {
+//if window is larger than 950px, animatePoints() runs
     if (window.innerHeight > 950) {
         animatePoints(pointsArray);
     }
@@ -15,6 +24,7 @@ window.onload = function () {
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
+//if window is smaller than 950px and user scrolls to selling points, animatePoints() runs
     window.addEventListener('scroll', function(event) {
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
             animatePoints(pointsArray);
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,11 +1,6 @@
-var forEach = function (array, callback) {
+//iterates through pointsArray and calls revealPoint() to style each point
+var forEach = function(array, callback) {
     for (var i = 0; i < array.length; i++) {
-        callback(array, i);
+        callback(array[i]);
     }
-}
-function myCallback (array, i) {
-    array[i].style.opacity = 1;
-    array[i].style.transform = "scale(1) translateY(0)"
-    array[i].style.msTransform = "scaleX(1) translateY(0)";
-    array[i].style.WebkitTransform = "scaleX(1) translateY(0)";
 }
